1c1943c4ec71bbae25631f77c58db77f2df57002
Ranjit Viswakumar <WhatsARanjit@users.noreply.github.com>
2018-11-29 19:49:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
 //--------------------------------------------------------------------
+// Variables
+variable "nodejs_security_group_id" {}
+variable "nodejs_vault_ip" {}
+
+//--------------------------------------------------------------------
 // Modules
 module "nodejs" {
   source  = "app.terraform.io/hashijit/nodejs/aws"
   version = "0.1.0"
 
-  prefix = "bcg"
+  prefix = "ibcg"
+  security_group_id = "${var.nodejs_security_group_id}"
   ssh_key_name = "ranjit-vault-demo"
+  vault_ip = "${var.nodejs_vault_ip}"
 }
