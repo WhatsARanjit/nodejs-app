3530ebcc8874cf5603ae53db5b06bb6454f1244a
Ranjit Viswakumar <WhatsARanjit@users.noreply.github.com>
2018-11-30 08:20:32 -0500

    Adding security group id

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "nodejs" {
   source  = "app.terraform.io/hashijit/nodejs/aws"
   version = "0.1.1"
 
+  security_group_id = "sg-0eb81199a3f8bc1ec"
   prefix = "bcg"
   ssh_key_name = "ranjit-vault-demo"
 }
